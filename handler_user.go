e45b5ec6503e334bb5d669b770bb30ed26d9f48b
Akshata Singh <singakshata1203@gmail.com>
2026-03-28 10:51:56 +0530

    server made

--- handler_user.go
+++ handler_user.go
@@ -41,8 +41,18 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 	log.Printf("User created with ID: %v", user.ID)
 }
 
-func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
-
-	
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {	
 	respondWithJSON(w, 200, databaseUsertoUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetPostsForUser(w http.ResponseWriter, r *http.Request, user database.User) {	
+	posts, err := apiCfg.DB.GetPostsForUser(r.Context(), database.GetPostsForUserParams{
+		UserID: user.ID,
+		Limit:  10,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Failed to get posts for user: %v", err))
+		return
+	}
+	respondWithJSON(w, 200, databasePostsToPosts(posts))
 }
--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"time"
 )
 
 type apiConfig struct {
@@ -20,7 +21,6 @@ type apiConfig struct {
 }
 
 func main() {
-
 	godotenv.Load(".env")
 	portString := os.Getenv("PORT")
 	if portString == "" {
@@ -37,9 +37,13 @@ func main() {
 		log.Fatal("Failed to connect to database:", err)
 	}
 
+	db:= database.New(conn)
+
 	apiCfg := apiConfig{
-		DB: database.New(conn),
+		DB: db,
 	}
+
+	go startScraping(db, 10, time.Minute)
 
 	router := chi.NewRouter()
 
@@ -58,6 +62,12 @@ func main() {
 	v1Router.Get("/err", handleErr)
 	v1Router.Post("/users", apiCfg.handlerCreateUser)
 	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
+	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
+	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
+	v1Router.Get("/posts", apiCfg.middlewareAuth(apiCfg.handlerGetPostsForUser))
+	v1Router.Post("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerCreateFeedFollow))
+	v1Router.Get("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerGetFeedFollows))
+	v1Router.Delete("/feed_follows/{feedFollowID}", apiCfg.middlewareAuth(apiCfg.handlerDeleteFeedFollow))
 
 
 	router.Mount("/v1", v1Router)
@@ -70,5 +80,6 @@ func main() {
 	if err = srv.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
+	
 
 }
